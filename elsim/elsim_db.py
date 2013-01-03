9c7b153092b761fbd82cf5ff63f28dd2dabb187a
desnos <desnos@localhost>
2013-01-03 09:29:41 +0100

    fix issues

--- elsim/elsim_db.py
+++ elsim/elsim_db.py
@@ -18,8 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Elsim.  If not, see <http://www.gnu.org/licenses/>.
 
-
-import logging, re
+import re
 
 from similarity.similarity import DBFormat, simhash
 from androguard.core.analysis import analysis
